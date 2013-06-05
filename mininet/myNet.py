cfd30b7049b3bb889646287166f264328504bb68
Zhu Nathan <nathan.zhu10@gmail.com>
2013-06-05 23:46:21 +0800

    修改mininet的py文件，自定义了一个较复杂的试验网络
    e.g. sudo python myNet.py 192.168.221.1

--- mininet/myNet.py
+++ mininet/myNet.py
@@ -4,23 +4,36 @@ from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
 from mininet.node import RemoteController
+import sys
 
 class MyTopo(Topo):
     "Single switch connected to n hosts."
     def __init__(self, **opts):
         # Initialize topology and default options
         Topo.__init__(self, **opts)
-	# Add switch
-        switch = self.addSwitch('s1')
+		# Add switch
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
         # Add hosts
-        for h in range(4):
-            host = self.addHost('h%s' % (h + 1), mac='00:00:00:00:00:0%s' % (h+1))
-            self.addLink(host, switch , 1 , h+1)
+        h1 = self.addHost('h1', mac='00:00:00:00:01:01', ip='10.0.1.1')
+        h21 = self.addHost('h21', mac='00:00:00:00:02:01', ip='10.0.2.1')
+        h22 = self.addHost('h22', mac='00:00:00:00:02:02',ip='10.0.2.2')
+        h31 = self.addHost('h31', mac='00:00:00:00:03:01',ip='10.0.3.1')
+        h32 = self.addHost('h32', mac='00:00:00:00:03:02',ip='10.0.3.2')
+        # Add link
+        self.addLink(s1, h1, 1, 1)
+        self.addLink(s1, s2, 2, 1)
+        self.addLink(s1, s3, 3, 1)
+        self.addLink(s2, h21, 2, 1)
+        self.addLink(s2, h22, 3, 1)
+        self.addLink(s3, h31, 2, 1)
+        self.addLink(s3, h32, 3, 1)
 
-def MyTest():
-    "Create and test a simple network"
+def MyTest(_ip='192.168.221.1'):
+    print 'controller is ' + _ip
     topo = MyTopo()
-    net = Mininet( topo=topo, controller=lambda name: RemoteController( name, ip='192.168.86.1' ) )
+    net = Mininet( topo=topo, controller=lambda name: RemoteController( name, ip=_ip ) )
     net.start()
     host = net.get('h1')
     CLI(net)
@@ -29,4 +42,5 @@ def MyTest():
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
-    MyTest()
+    ip = sys.argv[1]
+    MyTest(ip)
